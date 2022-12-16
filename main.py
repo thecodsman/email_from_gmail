a247a69e8a636842f20644c1ed744103d5afe5da
DawnofDonuts <34220198+thecodsman@users.noreply.github.com>
2022-12-16 09:49:10 -0500

    added unit testing. so now you can unit test :)

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import imaplib
 import time
 import email
 import re
+import unittest
+from unittest import TestCase
+from unittest import mock
 from collections import Counter
 from email_config import user, password
 
@@ -27,7 +30,6 @@ def ask_user_for_filters():
     useDateRange = input('use date range? (yes/no): ')
     if useDateRange.lower() == 'yes': 
         useDateRange = True
-        print('The date format is D-MMM-YYYY (eg. 5-Sep-2022 or 12-Aug-2022)')
         startDate = input('Enter start date: ')
         endDate = input('Enter end date: ')
 
@@ -113,30 +115,95 @@ def get_most_common_words(string):
     return top10Words
 
 # ===============================================================================================================================
+
+# unit test
+class TestEveryThing(TestCase):
+    @mock.patch('builtins.input')
+    def test_filter_by_recipient(self, mocked_input):
+        mocked_input.side_effect = ['yes', 'test@example.com', 'yes', '1-Jan-2022', '31-Dec-2022']
+        filters = ask_user_for_filters()
+        self.assertTrue(filters['UserFilter'])
+        self.assertEqual(filters['UserFilter'], 'test@example.com')
+        self.assertTrue(filters['StartDate'])
+        self.assertTrue(filters['EndDate'])
+        self.assertEqual(filters['StartDate'], '1-Jan-2022')
+        self.assertEqual(filters['EndDate'], '31-Dec-2022')
+
+    @mock.patch('builtins.input')
+    def test_no_filter_by_recipient(self, mocked_input):
+        mocked_input.side_effect = ['no', 'no']
+        filters = ask_user_for_filters()
+        self.assertFalse(filters['UserFilter'])
+        self.assertEqual(filters['UserFilter'], 0)
+        self.assertFalse(filters['StartDate'])
+        self.assertFalse(filters['EndDate'])
+        self.assertEqual(filters['StartDate'], 0)
+        self.assertEqual(filters['EndDate'], 0)
+    
+    def test_select_mail_with_filter(self):
+        imap = imaplib.IMAP4_SSL('imap.gmail.com')
+        imap.login(user, password)
+        messages = select_correct_mail(imap, '"[Gmail]/All Mail"', 'todd@spicytg.com', 0, 0)
+        print("if this one failed it might just be because you have not sent any emails to todd@spicytg.com")
+        self.assertTrue(messages)
+        imap.close()
+        imap.logout()
+
+    def test_select_mail_without_filter(self):
+        imap = imaplib.IMAP4_SSL('imap.gmail.com')
+        imap.login(user, password)
+        messages = select_correct_mail(imap, '"[Gmail]/All Mail"', 0, 0, 0)
+        self.assertTrue(messages)
+        imap.close()
+        imap.logout()
+
+    def test_get_all_email_with_filter(self):
+        imap = imaplib.IMAP4_SSL('imap.gmail.com')
+        imap.login(user, password)
+        messages = select_correct_mail(imap, '"[Gmail]/All Mail"', 'todd@spicytg.com', 0, 0)
+        self.assertTrue(messages)
+        allEmail, subjectsAndDates = get_emails(imap, messages)
+        self.assertTrue(allEmail)
+        self.assertTrue(subjectsAndDates)
+
+    def test_get_all_email_with_date_range(self):
+        imap = imaplib.IMAP4_SSL('imap.gmail.com')
+        imap.login(user, password)
+        messages = select_correct_mail(imap, '"[Gmail]/All Mail"', 0, '1-Oct-2022', '15-Dec-2022')
+        self.assertTrue(messages)
+        allEmail, subjectsAndDates = get_emails(imap, messages)
+        self.assertTrue(allEmail)
+        self.assertTrue(subjectsAndDates)
+
 # final stretch
 if __name__ == "__main__":
-    # connect
-    imap = imaplib.IMAP4_SSL("imap.gmail.com")
-    # login
-    imap.login(user, password)
+    useUnitTest = input("Use unit test? (yes/no): ")
+    if useUnitTest.lower() == 'yes':
+        unittest.main()
+    else:
+        # connect
+        imap = imaplib.IMAP4_SSL("imap.gmail.com")
+        # login
+        imap.login(user, password)
 
-    # get all the emails with the right filters and stuff
-    filters = ask_user_for_filters()
-    messages = select_correct_mail(imap, '"[Gmail]/All Mail"', filters['UserFilter'], filters['StartDate'], filters['EndDate'])
+        # get all the emails with the right filters and stuff
+        print('The date format is D-MMM-YYYY (eg. 5-Sep-2022 or 12-Aug-2022)')
+        filters = ask_user_for_filters()
+        messages = select_correct_mail(imap, '"[Gmail]/All Mail"', filters['UserFilter'], filters['StartDate'], filters['EndDate'])
 
-    print('getting your emails...\n')
-    allEmail, subjectsAndDates = get_emails(imap, messages)
-    
-    # print the subjects and dates they were sent
-    for subject, date in subjectsAndDates.items():
-        print(f'Subject: {subject}      Date: {date}\n{"-"*50}')
-        time.sleep(0.1) # wait a tenth of a second before printing the next subject and date
+        print('getting your emails...\n')
+        allEmail, subjectsAndDates = get_emails(imap, messages)
+        
+        # print the subjects and dates they were sent
+        for subject, date in subjectsAndDates.items():
+            print(f'Subject: {subject}      Date: {date}\n{"-"*50}')
+            time.sleep(0.1) # wait a tenth of a second before printing the next subject and date
 
-    # get top 10 words
-    top10Words = get_most_common_words(allEmail)
+        # get top 10 words
+        top10Words = get_most_common_words(allEmail)
 
-    # print top 10 words
-    print('\ntop 10 words:\n')
-    for word, count in top10Words:
-        print(f'{word}: {count}')
+        # print top 10 words
+        print('\ntop 10 words:\n')
+        for word, count in top10Words:
+            print(f'{word}: {count}')
 
